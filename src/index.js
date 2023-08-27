4a6e75698fe615dd7c7f7abccac6fc0bb6fb15f9
Yuliya <kuzmich.yuliya@gmail.com>
2023-08-27 15:12:28 +0200

    add: dozen numbers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-//module.exports = 
-function toReadable (number) {
-    
-    let numUpToTwenty = {
+module.exports = function toReadable (number) {    
+    /*let numTwenty = new Map([
+       [0, 'zero'],
+       [1, 'one'],
+        [2, 'two'],
+        [3, 'three'],
+        [4, "four"],
+        [5, 'five'],
+        [6, 'six'],
+        [7, "seven"],
+        [8, 'eight'],
+        [9, 'nine'],
+        [10, 'ten'],
+        [11, 'eleven'],
+        [12, 'twelve'],
+        [13, 'thirteen'],
+        [14, 'fourteen'],
+        [15, 'fifteen'],
+        [16, 'sixteen'],
+        [17, 'seventeen'],
+        [18, 'eightieen'],
+        [19, 'nineteen'],
+       
+    ])
+    console.log(numTwenty)
+    return numTwenty.get(number);
+   */
+    const numTwenty = {
         0: 'zero',
         1: 'one',
         2: 'two',
@@ -20,10 +44,20 @@ function toReadable (number) {
         15: 'fifteen',
         16: 'sixteen',
         17: 'seventeen',
-        18: 'eightieen',
+        18: 'eighteen',
         19: 'nineteen',
        
-    };
-    //console.log(Object.keys(numUpToTwenty));
-    
+    }
+    return numTwenty[number.toString()]
+
+    const twentyPlus = {
+        20: 'twenty',
+        30: 'thirty',
+        40: 'thirty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninty',
+    }
 }
